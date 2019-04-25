291cb47c24e58d23905577a01553afc2a4d3ddb1
Jorge Sierra <jorge.sierra.florido@gmail.com>
2019-04-24 20:04:25 -0500

    test

---
+++ customcomponentslibrary/src/main/java/com/chemasmas/customcomponentslibrary/Util.kt
@@ -0,0 +1,13 @@
+package com.chemasmas.customcomponentslibrary
+
+import android.util.Log
+import android.widget.Toast
+
+class Util{
+
+    companion object{
+        fun health() {
+            Log.d("AUTHOR","CHEMASMAS")
+        }
+    }
+}
